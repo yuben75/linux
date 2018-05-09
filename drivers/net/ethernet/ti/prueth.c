altenstef/ti-linux

net: driver: ti: prueth: handling HSR PTP event cut-through tx timestamp
When a HSR non-link-local PTP event is cut-through forward by the
HSR firmware, in addition to the rx timestamp, a tx timestamp is
also provided by the firmware. The tx timestamp is saved in the
shared redundant info area of the received skb before the skb
is passed to upper layer. Currently cut-through tx timestamp is
handled only for HSR PTP over L2 non-link-local event.

tmp: iep: hsr ptp cut-through tx ts processing
tmp: prueth: hsr ptp cut-through tx ts processing
tmp: prueth: passing redinfo hwtstamp to user sapce

Signed-off-by: WingMan Kwok <w-kwok2@ti.com>

drivers/net/ethernet/ti/prueth.c
@@ -95,6 +95,12 @@ static char *pruss2_mc_mask = PRUETH_DEFAULT_MC_MASK;
 module_param(pruss2_mc_mask, charp, 0444);
 MODULE_PARM_DESC(pruss2_mc_mask, "Choose pruss2 MC mask");
 
+static inline bool is_ptp_cut_through_event(u8 ts_msgtype)
+{
+	return (ts_msgtype == PTP_SYNC_MSG_ID) ||
+	       (ts_msgtype == PTP_DLY_REQ_MSG_ID);
+}
+
 static inline int is_hsr_skb(struct sk_buff *skb)
 {
 	unsigned char *p;
@@ -243,6 +249,15 @@ static int pruptp_proc_tx_ts(struct prueth_emac *emac,
 	emac->tx_ev_msg[ts_msgtype] = NULL;
 	spin_unlock_irqrestore(&emac->ev_msg_lock, flags);
 	if (!skb) {
+		/* In case of HSR, tx timestamp may be generated by
+		 * cut-through packets such as SYNC, which does not
+		 * have a corresponding queued tx packet. Such a tx
+		 * timestamp is consumed by the rx port when processing
+		 * the rx timestamp.
+		 */
+		if (PRUETH_HAS_HSR(prueth))
+			return 0;
+
 		netdev_err(emac->ndev,
 			   "no tx msg %u found waiting for ts\n", ts_msgtype);
 		return -ENOMSG;
@@ -360,6 +375,36 @@ static void prueth_clean_ptp_tx_work(void) { }
 
 #endif /* !CONFIG_PREEMPT_RT_FULL */
 
+static int pruptp_hsr_cut_through_tx_ts(struct prueth_emac *emac,
+					struct sk_buff *skb,
+					u8 ts_msgtype)
+{
+	struct prueth *prueth = emac->prueth;
+	struct prueth_emac *other_emac = prueth->emac[emac->port_id ^ 0x3];
+	int other_port_id = other_emac->port_id;
+	u32 ts_ofs;
+	u64 ns;
+	struct skb_shared_hwtstamps *red_ssh;
+
+	ts_ofs = TX_SYNC_TIMESTAMP_OFFSET_P1 +
+		 (other_port_id - 1) * NUM_TS_EVENTS * TS_FIELD_SIZE;
+
+	if (ts_msgtype == PTP_DLY_REQ_MSG_ID)
+		ts_ofs += TS_FIELD_SIZE;
+
+	iep_get_timestamp(prueth->iep, ts_ofs, &ns);
+
+	/* +++TODO: What if the cut-through tx timestamp is
+	 * not available yet?
+	 */
+
+	/* Save the cut-through tx ts in skb redinfo. */
+	red_ssh = skb_redinfo_hwtstamps(skb);
+	memset(red_ssh, 0, sizeof(*red_ssh));
+	red_ssh->hwtstamp = ns_to_ktime(ns);
+	return 0;
+}
+
 static int pruptp_rx_timestamp(struct prueth_emac *emac, struct sk_buff *skb)
 {
 	struct prueth *prueth = emac->prueth;
@@ -404,6 +449,15 @@ static int pruptp_rx_timestamp(struct prueth_emac *emac, struct sk_buff *skb)
 	if (ret < 0) {
 		netdev_err(emac->ndev, "invalid timestamp for rx msg %u\n",
 			   ts_msgtype);
+		return ret;
+	}
+
+	if (PRUETH_HAS_HSR(prueth) && is_ptp_cut_through_event(ts_msgtype)) {
+		/* Received SYNC in HSR mode. That means a copy of it is
+		 * cut-through by fw and a tx ts is available on the other
+		 * port. So save the tx ts in skb's shared redinfo
+		 */
+		pruptp_hsr_cut_through_tx_ts(emac, skb, ts_msgtype);
 	}
 
 	return ret;

yuben75/linux

drivers/net/ethernet/ti/prueth.c
@@ -106,6 +106,12 @@ static inline enum prueth_port other_port_id(enum prueth_port port_id)
 	return other_port_id;
 }
 
+static inline bool is_ptp_cut_through_event(u8 ts_msgtype)
+{
+	return (ts_msgtype == PTP_SYNC_MSG_ID) ||
+	       (ts_msgtype == PTP_DLY_REQ_MSG_ID);
+}
+
 static inline int is_hsr_skb(struct sk_buff *skb)
 {
 	unsigned char *p;
@@ -254,6 +260,15 @@ static int pruptp_proc_tx_ts(struct prueth_emac *emac,
 	emac->tx_ev_msg[ts_msgtype] = NULL;
 	spin_unlock_irqrestore(&emac->ev_msg_lock, flags);
 	if (!skb) {
+		/* In case of HSR, tx timestamp may be generated by
+		 * cut-through packets such as SYNC, which does not
+		 * have a corresponding queued tx packet. Such a tx
+		 * timestamp is consumed by the rx port when processing
+		 * the rx timestamp.
+		 */
+		if (PRUETH_HAS_HSR(prueth))
+			return 0;
+
 		netdev_err(emac->ndev,
 			   "no tx msg %u found waiting for ts\n", ts_msgtype);
 		return -ENOMSG;
@@ -371,6 +386,35 @@ static void prueth_clean_ptp_tx_work(void) { }
 
 #endif /* !CONFIG_PREEMPT_RT_FULL */
 
+static int pruptp_hsr_cut_through_tx_ts(struct prueth_emac *emac,
+					struct sk_buff *skb,
+					u8 ts_msgtype)
+{
+	struct prueth *prueth = emac->prueth;
+	int pair_port_id = other_port_id(emac->port_id);
+	u32 ts_ofs;
+	u64 ns;
+	struct skb_shared_hwtstamps *red_ssh;
+
+	ts_ofs = TX_SYNC_TIMESTAMP_OFFSET_P1 +
+		 (pair_port_id - 1) * NUM_TS_EVENTS * TS_FIELD_SIZE;
+
+	if (ts_msgtype == PTP_DLY_REQ_MSG_ID)
+		ts_ofs += TS_FIELD_SIZE;
+
+	iep_get_timestamp(prueth->iep, ts_ofs, &ns);
+
+	/* +++TODO: What if the cut-through tx timestamp is
+	 * not available yet?
+	 */
+
+	/* Save the cut-through tx ts in skb redinfo. */
+	red_ssh = skb_redinfo_hwtstamps(skb);
+	memset(red_ssh, 0, sizeof(*red_ssh));
+	red_ssh->hwtstamp = ns_to_ktime(ns);
+	return 0;
+}
+
 static int pruptp_rx_timestamp(struct prueth_emac *emac, struct sk_buff *skb)
 {
 	struct prueth *prueth = emac->prueth;
@@ -415,6 +459,15 @@ static int pruptp_rx_timestamp(struct prueth_emac *emac, struct sk_buff *skb)
 	if (ret < 0) {
 		netdev_err(emac->ndev, "invalid timestamp for rx msg %u\n",
 			   ts_msgtype);
+		return ret;
+	}
+
+	if (PRUETH_HAS_HSR(prueth) && is_ptp_cut_through_event(ts_msgtype)) {
+		/* Received SYNC in HSR mode. That means a copy of it is
+		 * cut-through by fw and a tx ts is available on the other
+		 * port. So save the tx ts in skb's shared redinfo
+		 */
+		pruptp_hsr_cut_through_tx_ts(emac, skb, ts_msgtype);
 	}
 
 	return ret;