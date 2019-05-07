c3439598350e0e168a7120c3c60e665b46fb951c
Murali Karicheri <m-karicheri2@ti.com>
2019-05-07 10:33:31 -0400

    net: hsr/prp: add vlan support
    This patch add support for creating vlan interfaces
    over hsr/prp interface.
    
    Signed-off-by: Murali Karicheri <m-karicheri2@ti.com>

--- net/hsr-prp/hsr_prp_slave.c
+++ net/hsr-prp/hsr_prp_slave.c
@@ -19,7 +19,6 @@ static rx_handler_result_t handle_frame(struct sk_buff **pskb)
 	struct sk_buff *skb = *pskb;
 	struct hsr_prp_port *port;
 	struct hsr_prp_priv *priv;
-
 	u16 protocol;
 
 	rcu_read_lock(); /* hsr->node_db, hsr->ports */
@@ -38,10 +37,11 @@ static rx_handler_result_t handle_frame(struct sk_buff **pskb)
 		goto finish_consume;
 	}
 
-	/* For HSR, non tagged frames are expected, but for PRP
+	/* For HSR, non tagged frames are unexpected, but for PRP
 	 * there could be non tagged frames as well.
 	 */
 	protocol = eth_hdr(skb)->h_proto;
+
 	if (protocol != htons(ETH_P_PRP) &&
 	    protocol != htons(ETH_P_HSR) &&
 	    port->priv->prot_version <= HSR_V1 &&
@@ -102,11 +102,6 @@ static int check_dev_ok(struct net_device *dev)
 
 	if (hsr_prp_port_exists(dev)) {
 		netdev_info(dev, "This device is already a HSR slave.\n");
-		return -EINVAL;
-	}
-
-	if (is_vlan_dev(dev)) {
-		netdev_info(dev, "HSR on top of VLAN is not yet supported in this driver.\n");
 		return -EINVAL;
 	}
 
